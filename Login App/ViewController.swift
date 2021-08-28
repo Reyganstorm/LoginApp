8cc4602387660e9f7a5c4ea49e8d5af4cf3e7ac6
Ruslan Shtybaev <ruslan.shtybaev@gmail.com>
2021-08-28 17:17:26 +0300

    Add design , new ViewControler, Alert, algorithm

--- Login App/ViewController.swift
+++ Login App/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    // MARK: - IB Outlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordNextField: UITextField!
+    
+    // MARK: Private Properties
+    private let user = "User"
+    private let password = "Password"
+    
+    @IBAction func forgotRegistredData(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(title: "Oops", message: "Your name is \(user).")
+            : showAlert(title: "Oops", message: "Your password id \(password).")
     }
 
 
 }
 
+// MARK: - Private Methods
+extension ViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(
+            title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+}
