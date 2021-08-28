f2cd42fb4ce4037f9d11ce257adb3fef17a01721
Ruslan Shtybaev <ruslan.shtybaev@gmail.com>
2021-08-28 22:26:51 +0300

    Finished version without transition across "done"

--- Login App/ViewController.swift
+++ Login App/ViewController.swift
@@ -11,20 +11,42 @@ class ViewController: UIViewController {
     
     // MARK: - IB Outlets
     @IBOutlet var userNameTextField: UITextField!
-    @IBOutlet var passwordNextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
     
     // MARK: Private Properties
     private let user = "User"
     private let password = "Password"
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else {
+            return
+        }
+        welcomeVC.user = user
+    }
+
+    // MARK: IBActions
+    @IBAction func logInPressed() {
+        if userNameTextField.text != user || passwordTextField.text != password {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password."
+            )
+        }
+    }
     
     @IBAction func forgotRegistredData(_ sender: UIButton) {
         sender.tag == 0
             ? showAlert(title: "Oops", message: "Your name is \(user).")
             : showAlert(title: "Oops", message: "Your password id \(password).")
     }
-
-
-}
+    
+    @IBAction func unwindSegue( segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    }
 
 // MARK: - Private Methods
 extension ViewController {
@@ -38,4 +60,22 @@ extension ViewController {
         present(alert, animated: true)
     }
     
+}
+
+// MARK:
+extension ViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            logInPressed()
+            performSegue(withIdentifier: "ShowWelcomeVC", sender: nil)
+        }
+        return true
+    }
 }
--- Login App/WelcomeViewController.swift
+++ Login App/WelcomeViewController.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var user  = ""
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        private let primaryColor = UIColor(
+            red: 200/255,
+            green: 120/255,
+            blue: 80/255,
+            alpha: 1
+        )
+        private let secondaryColor = UIColor(
+            red: 80/255,
+            green: 148/255,
+            blue: 230/255,
+            alpha: 1
+        )
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+            welcomeLabel.text = "Welcome, \(user)!"
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
-    */
-
 }
